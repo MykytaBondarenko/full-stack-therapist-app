2632dac25d4d925a3b50c56e67e0c77d5eefa6c0
Mykyta <mykyta.bondarenko.2024@mumail.ie>
2025-04-08 16:13:05 +0100

    Functional clients page

--- backend/server.js
+++ backend/server.js
@@ -3,11 +3,15 @@ const cors = require('cors');
 const bodyParse = require('body-parser');
 require('dotenv').config();
 
+const clientsRouter = require("./routers/clientsRouter.js");
+
 const server = express();
 server.use(cors());
 server.use(bodyParse.json());
 const PORT = process.env.PORT;
 
+server.use(clientsRouter);
+
 server.listen(PORT, () => {
-    console.log('Server running https://localhost:' + PORT);
+    console.log('Server running http://localhost:' + PORT);
 });
--- frontend/src/components/Clients/Clients.js
+++ frontend/src/components/Clients/Clients.js
@@ -1,7 +1,62 @@
 import './Clients.css';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 export default function Clients() {
+    const [clientsData, setClientsData] = useState([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+
+    function nameInputHandler(e) {
+        fetchClients(e.target.value);
+    }
+
+    useEffect(() => {
+        fetchClients();
+    }, []);
+
+    function fetchClients(clientName) {
+        axios
+            .get("http://localhost:5000/clients/", {
+                params: {
+                    name: clientName
+                }
+            })
+            .then((response) => {
+                setClientsData(response.data);
+                setLoading(false);
+            })
+            .catch((error) => {
+                setError(error.message);
+                setLoading(false);
+            });
+    }
+
+    if (loading) return (<div>Loading...</div>);
+    if (error) return (<div>Error: {error}</div>);
+
+    let clientsList = getClientsList();
+
+    function getClientsList() {
+        let clientsList;
+        if (clientsData.length < 1) clientsList = "Couldn't find the client";
+        else clientsList = clientsData.map(client => <li class="clientBox">{client.name}
+                                                        <p>Email: {client.email}</p>
+                                                        <p>Phone No: {client.phone_number}</p>
+                                                        <p>Regularity: {client.regularity}</p>
+                                                    </li>);
+        return clientsList;
+    }
+
     return (
-        <h1>Clients page</h1>
+        <div>
+            <h1>Clients page</h1>
+            <div id="inputDiv">
+                <div>
+                    <input type="text" id="clientName" placeholder="Client's Name" class="findClient" onChange={(nameInputHandler)}></input>
+                </div>
+            </div>
+            <ul id="clientsUL">{clientsList}</ul>
+        </div>
     )
 }
